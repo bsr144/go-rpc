0a60a6aa4e088912f8d999e93489ffd8c7e511bc
abraham-koinworks <abraham.purnomo@koinworks.com>
2024-01-03 13:08:54 +0700

    init push local to remote

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"go-rpc/client"
+	"go-rpc/proto"
+	"log"
+	"time"
+)
+
+func main() {
+	var (
+		jsonAddr = flag.String("json", ":3000", "listen address of the json transport")
+		grpcAddr = flag.String("grpc", ":4000", "listen address of the grpc transport")
+		svc      = loggingService{&priceService{}}
+		ctx      = context.Background()
+	)
+	flag.Parse()
+
+	grpcClient, err := client.NewGRPCClient(":4000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			time.Sleep(3 * time.Second)
+			resp, err := grpcClient.FetchPrice(ctx, &proto.PriceRequest{Ticker: "BTC"})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%+v\n", resp)
+		}
+	}()
+
+	go makeGRPCServerAndRun(*grpcAddr, svc)
+
+	jsonServer := NewJSONAPIServer(*jsonAddr, svc)
+	jsonServer.Run()
+}
